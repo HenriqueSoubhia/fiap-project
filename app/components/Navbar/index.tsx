813b418064a43be88a45b6de15a2063c3a4be915
HenriqueSoubhia <soubhiahenrique0@gmail.com>
2025-04-28 10:27:50 -0300

    base nav done

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,18 +1,25 @@
 "use client"
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import styled from 'styled-components'
 
 const StyledNav = styled.nav`
-    background-color: ${({ theme }) => theme.colors.chineseBlack};   
+    background-color: ${({ theme }) => theme.colors.chineseBlack};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;   
+    width: 100%;   
 `
 
 const Navbar = () => {
     return (
         <StyledNav>
-            <Image src={'/svg/logo.svg'} alt='Logo FIAP' width={86} height={25} />
-
+            <Link href={'/'}>
+                <Image src={'/svg/logo.svg'} alt='Logo FIAP' width={86} height={25} />
+            </Link>
 
         </StyledNav>
     )
