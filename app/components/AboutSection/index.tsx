c9ab8e7029a4570a0ae2df203faebb0c885caa2c
HenriqueSoubhia <soubhiahenrique0@gmail.com>
2025-04-28 15:18:57 -0300

    about section almost done

--- app/components/AboutSection/index.tsx
+++ app/components/AboutSection/index.tsx
@@ -1,68 +1,151 @@
 import Image from 'next/image'
 import React from 'react'
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+    display:flex;
+    flex-direction:column;
+    gap: 3rem;
+    padding: 200px 90px;
+    background-color: ${({ theme }) => theme.colors.cultured};
+`;
+
+const StyledH2 = styled.h2`
+    font-size: 4rem;
+    text-transform: uppercase;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.black};
+
+`
+
+const StyledArticle = styled.article`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 90px; 
+    gap: 120px;
+    color: ${({ theme }) => theme.colors.davysGrey};
+    text-align: justify;
+
+`
+
+const StyledImageDiv = styled.div`
+    display: flex;
+    flex-direction: column ;
+    gap:50px;
+`
+
+const StyledFigure = styled.figure`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+
+const StyledListDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+const StyledMainList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+
+    > li::before{
+        content: '/';
+        margin: 0 16px;
+        font-weight: 700;
+        color: ${({ theme }) => theme.colors.magenta};
+    }
+    
+`
+const StyledTechList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 20px;
+
+    
+
+    li{
+        font-size: .9rem;
+        color: ${({ theme }) => theme.colors.magenta};
+        &::before{
+            content: '-';
+            margin-left: 32px;
+            margin-right: 12px;
+            font-weight: 700;
+            color: ${({ theme }) => theme.colors.magenta};
+        }
+    }
+    
+`
+
 
 const AboutSection = () => {
     return (
-        <section>
-            <h2>educação e tech</h2>
+        <StyledSection>
+            <StyledH2>educação e tech</StyledH2>
 
-            <div>
-                <p>
-                    A FIAP é muito mais do que uma faculdade de tecnologia, é um verdadeiro ecossistema de inovação e aprendizado. Conhecida por sua abordagem moderna e focada no mercado, a instituição é referência quando se trata de formar profissionais capacitados para os desafios do futuro. Seja na graduação, pós-graduação ou cursos livres, a FIAP entrega um ensino que mistura teoria e prática, preparando os alunos para atuar em empresas de ponta ou até mesmo empreender.
-                </p>
+            <StyledArticle>
+                <StyledImageDiv>
+                    <p>
+                        A FIAP é muito mais do que uma faculdade de tecnologia, é um verdadeiro ecossistema de inovação e aprendizado. Conhecida por sua abordagem moderna e focada no mercado, a instituição é referência quando se trata de formar profissionais capacitados para os desafios do futuro. Seja na graduação, pós-graduação ou cursos livres, a FIAP entrega um ensino que mistura teoria e prática, preparando os alunos para atuar em empresas de ponta ou até mesmo empreender.
+                    </p>
 
-                <Image src="" alt='Logotipo FIAP Next' />
+                    <StyledFigure>
+                        <img src="/imgs/fiap-next.jpg" alt='Logotipo FIAP Next' />
 
-                <small>
-                    Figura 1 - Logotipo da FIAP NEXT
-                </small>
-            </div>
+                        <figcaption>
+                            Figura 1 - Logotipo da FIAP NEXT
+                        </figcaption>
+                    </StyledFigure>
+                </StyledImageDiv>
 
-            <div>
-                <p>
-                    Com um corpo docente formado por profissionais atuantes no mercado, a FIAP não ensina apenas tecnologia, mas também negócios, inovação e liderança. Seus cursos abrangem diversas áreas, incluindo:
-                </p>
+                <StyledListDiv>
+                    <p>
+                        Com um corpo docente formado por profissionais atuantes no mercado, a FIAP não ensina apenas tecnologia, mas também negócios, inovação e liderança. Seus cursos abrangem diversas áreas, incluindo:
+                    </p>
 
 
-                <ul>
-                    <li>
-                        <span>
+                    <StyledMainList>
+                        <li>
                             Tecnologia e Desenvolvimento
-                        </span>
 
-                        <ul>
-                            <li>
-                                Desenvolvimento de Software
-                            </li>
-                            <li>
-                                Inteligência Artificial
-                            </li>
-                            <li>
-                                Data Science
-                            </li>
+                            <StyledTechList>
+                                <li>
+                                    Desenvolvimento de Software
+                                </li>
+                                <li>
+                                    Inteligência Artificial
+                                </li>
+                                <li>
+                                    Data Science
+                                </li>
 
-                            <li>
-                                Cibersegurança
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        UX/UI, Marketing Digital
-                    </li>
-                    <li>
-                        Metodologias e Gestão
-                    </li>
-                </ul>
+                                <li>
+                                    Cibersegurança
+                                </li>
+                            </StyledTechList>
+                        </li>
+                        <li>
+                            UX/UI, Marketing Digital
+                        </li>
+                        <li>
+                            Metodologias e Gestão
+                        </li>
+                    </StyledMainList>
 
-                <p>
-                    Além disso, a instituição incentiva fortemente o uso dessas metodologias e outras abordagens inovadoras que fazem parte do dia a dia de grandes empresas.
-                </p>
-            </div>
+                    <p>
+                        Além disso, a instituição incentiva fortemente o uso dessas metodologias e outras abordagens inovadoras que fazem parte do dia a dia de grandes empresas.
+                    </p>
+                </StyledListDiv>
+
+            </StyledArticle>
 
 
 
-
-        </section>
+        </StyledSection>
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 'use client'
 
+import AboutSection from "./components/AboutSection";
 import HeaderBanner from "./components/HeaderBanner";
 import Navbar from "./components/Navbar";
 
 
 export default function Home() {
 
-  
+
   return (
     <div >
       <Navbar />
-      <HeaderBanner />
-     
+      <main>
+        <HeaderBanner />
+        <AboutSection />
+      </main>
+
     </div>
   );
 }
