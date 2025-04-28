a2802938e81c8f0bb15257a6f9b51ce0de552722
HenriqueSoubhia <soubhiahenrique0@gmail.com>
2025-04-28 09:02:57 -0300

    fix: gotham font

--- app/components/GlobalStyle/index.tsx
+++ app/components/GlobalStyle/index.tsx
@@ -1,21 +1,22 @@
 import { createGlobalStyle } from "styled-components"
 
 
-export const GlobalStyle = createGlobalStyle`
 
+export const GlobalStyle = createGlobalStyle`
 
   /* FONTES */
 
   @font-face {
-    font-family: 'GothamHTF';
-    src: url('/fonts/gotham/GothamHTF-Medium.otf') format('otf');
-    font-weight: 400;
-    font-style: normal;
-  }
+  font-family: 'GothamHTF';
+  src: url('/fonts/gotham/GothamHTF-Medium.otf') format('opentype');
+  font-weight: 400;
+  font-style: normal;
+}
+
 
   @font-face {
     font-family: 'GothamHTF';
-    src: url('/fonts/gotham/GothamHTF-Bold.otf') format('otf');
+    src: url('/fonts/gotham/GothamHTF-Bold.otf') format('opentype');
     font-weight: 600;
     font-style: normal;
   }
@@ -31,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
   html, body {
     height: 100%;
     width: 100%;
-    font-family: 'SuaFonte', sans-serif;
+    font-family: 'GothamHTF', sans-serif;
     background: #fff;
     color: #000;
     -webkit-font-smoothing: antialiased;
