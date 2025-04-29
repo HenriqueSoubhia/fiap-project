f121dfe4c4c5ee76308edab24cc90be4ee70f3ae
HenriqueSoubhia <soubhiahenrique0@gmail.com>
2025-04-29 11:22:36 -0300

    bg circles accordion section

--- app/components/AccordionSection/index.tsx
+++ app/components/AccordionSection/index.tsx
@@ -8,6 +8,26 @@ const StyledAccordionSection = styled.section`
     padding: 200px 90px;
     background-color: ${({ theme }) => theme.colors.black};
     position: relative;
+    z-index: 1;
+
+    &:before, &:after {
+        content: '';
+        position: absolute;
+        width: 450px;
+        height: 450px;
+        background-color: rgba(0, 107, 183, 0.2);
+        border-radius: 50%;
+        z-index: -1;
+        filter: blur(400px);
+    }
+
+    &:before {
+      left: -200px;
+    }
+    &:after {
+      right: -200px;
+      top: 300px;
+    }
 
     @media (max-width: 768px) {
         padding: 100px 24px;
@@ -25,10 +45,10 @@ const AccordionSection = () => {
   return (
     <StyledAccordionSection>
 
-      <Title style={{marginBottom: 75}} headingSize='h2' variant='pastelBlue'>nossa estrutura</Title>
+      <Title style={{ marginBottom: 75 }} headingSize='h2' variant='pastelBlue'>nossa estrutura</Title>
 
       {accordionsContent.map((item, index) => (
-        <Accordion key={index} title={item.title} desc={item.desc} index={index} handleAccordionToggle={handleAccordionToggle} isOpen={accordionOpen === index}/>
+        <Accordion key={index} title={item.title} desc={item.desc} index={index} handleAccordionToggle={handleAccordionToggle} isOpen={accordionOpen === index} />
       ))}
 
 
