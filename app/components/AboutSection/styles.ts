8d6b5af04a50a2895c34402c351f166456765cd6
HenriqueSoubhia <soubhiahenrique0@gmail.com>
2025-04-29 18:11:44 -0300

    move styles from image modal file

--- app/components/AboutSection/styles.ts
+++ app/components/AboutSection/styles.ts
@@ -85,6 +85,75 @@ const StyledTechList = styled.ul`
   }
 `;
 
+const StyledFigure = styled.figure`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+
+  img {
+    cursor: url('/svg/cursor-open.svg'), pointer;
+  }
+
+  figcaption {
+    font-size: 16px;
+  }
+
+  @media (max-width: 768px) {
+    figcaption {
+      font-size: 12px;
+    }
+  }
+`;
+
+const StyledDialog = styled.dialog`
+  border: none;
+  outline: none;
+
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 101;
+
+  justify-content: center;
+  align-items: center;
+
+  &[open] {
+    display: flex;
+  }
+
+  button {
+    position: fixed;
+    top: 32px;
+    right: 32px;
+    background: transparent;
+    border: none;
+    color: white;
+    font-size: 3rem;
+    cursor: pointer;
+    outline: none;
+    z-index: 101;
+  }
+
+  img {
+    height: 600px;
+    cursor: url('/svg/cursor-close.svg'), pointer;
+  }
+
+  @media (max-width: 768px) {
+    img {
+      height: auto;
+      width: 100vw;
+    }
+  }
+`;
+
 export {
   StyledSection,
   StyledArticle,
@@ -92,4 +161,6 @@ export {
   StyledListDiv,
   StyledMainList,
   StyledTechList,
+  StyledFigure,
+  StyledDialog,
 };
